77ff923dc8a5d4c6a49cef07cc8d242421a6b7fc
suannlim <suann9@gmail.com>
2020-12-04 14:29:36 +0000

    univariate method

--- neutrino_oscillation.py
+++ neutrino_oscillation.py
@@ -3,32 +3,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def readData(data,eventRate):
+def readData(data):
     """
     function to read in data from data files
 
     """
     #opening data files 
     fileObject=open(data,"r")
-    fileObject1=open(eventRate,"r")
+    fileObject1=open(data,"r")
 
     #read in data
-    experimentData=fileObject.read().split("\n")
-    eventRateData=fileObject1.read().split("\n")
+    experimentData=fileObject.readlines()[2:202]
+    simulatedData=fileObject1.readlines()[205:405]
+
 
     #appending data into arrays
     expData=[]
     for i in experimentData:
         expData.append(float(i))
-    rateData=[]
-    for i in eventRateData:
-        rateData.append(float(i))
+    simData=[]
+    for i in simulatedData:
+        simData.append(float(i))
+    
+    return(expData,simData)
 
-
-    return(expData,rateData)
-
-expData=readData("data.txt","eventRate.txt")[0]
-simData=readData("data.txt","eventRate.txt")[1]
+expData=readData("dataFile.txt")[0]
+simData=readData("dataFile.txt")[1]
 
 """
 Plotting the actual and predicted event rates
@@ -78,7 +78,7 @@ newE=np.linspace(0,10-0.05,200)
 
 
 #finding the probability of the neutrino not decaying
-noDecayProb=noOscProb(newE,np.pi/4,2.4e-3,295)
+noDecayProb=noOscProb(E,np.pi/4,2.4e-3,295)
 
 #plotting probability values on a graph
 plt.figure(1)
@@ -116,10 +116,11 @@ def negLogLike(expdata,simdata,mixAng):
     L=295
     E=np.arange(0.025,10,0.05)
     for i in mixAng:
+        #finding the array of probabilities for a specific mix ang (prob vals depend on E)
         noDecayProb=noOscProb(E,i,diffSqrMass,L)
         NLLsum=0
+        OscillationEventRate=oscEventRate(noDecayProb,simdata)
         for j in range(len(noDecayProb)):
-            OscillationEventRate=oscEventRate(noDecayProb,simdata)
             m=expdata[j]
             Lamda=OscillationEventRate[j]
             if m==0:
@@ -142,6 +143,8 @@ plt.xlabel("Mixing angle")
 plt.ylabel("Negative Log Likelihood")
 plt.plot(mixAng,likelihoodVals)
 
+print(mixAng,likelihoodVals)
+
 
 
 
@@ -149,7 +152,7 @@ plt.plot(mixAng,likelihoodVals)
 #%%
 #3.4 Minimisation
 
-
+#this minimiser is only effective when the approximate positionof the minimum is known
 def parabolicMinimiser(xVals,yVals):
     """
     Parabolic minimisation takes 3 points(x0,x1,x2) and fits a lagrange polynomial across
@@ -163,18 +166,28 @@ def parabolicMinimiser(xVals,yVals):
     #creating a dictionary between the x and y array
     dictionary=dict(zip(xVals,yVals))
 
-    #append the first, middle and last value
-    xGuess.append(xVals[70])
-    xGuess.append(xVals[80])
-    xGuess.append(xVals[90])
+    #need to find 3 initial points f(x1),f(x2) and f(x3) such that f(x2) is the lowest point
+    xGuess.append(xVals[10])
+    firstGuess=15
+    secGuess=20
+    while dictionary[xVals[firstGuess]]>= dictionary[xVals[secGuess]]:
+        firstGuess+=1
+        secGuess+=1
+    
+    xGuess.append(xVals[firstGuess])
+    xGuess.append(xVals[secGuess])
     
     #continue iterating until x3 changes by less than 0.001
     diffx3=100
     prevX = 0
 
-    while diffx3>0.00000001:
+    #store the xGuess values used to make the last polynomial
+    xFinal=[]
 
-        print(xGuess)
+    while diffx3>0.000001:
+
+        xFinal=xGuess
+
         #find all relevant y values
         y0=dictionary[xGuess[0]]
         y1=dictionary[xGuess[1]]
@@ -193,7 +206,6 @@ def parabolicMinimiser(xVals,yVals):
 
         #finding minimum of lagrange polynomial and appending to xGuess
         x3=0.5*(totalNum/totalDenom)
-        print("found x3: ", x3)
         diffx3=abs(x3 - prevX)
 
         #finding the y value corresponding to this x value
@@ -208,8 +220,6 @@ def parabolicMinimiser(xVals,yVals):
 
         xGuess.append(x3)
         prevX = x3
-        print("Setting prev3 to: ", prevX)
-        print(xGuess)
         
         #loop through xGuess to remove the x value that corresponds to the largest y
         maxY = 0
@@ -219,13 +229,13 @@ def parabolicMinimiser(xVals,yVals):
                 maxY=dictionary[i]
                 maxX=i
         xGuess.remove(maxX)
-        print(xGuess)
+
+        print(x3,y3)
         
-    print(x3)
-    return(x3,y3)
+    return(x3,y3,dictionary,xFinal)
 
 
-
+plt.figure("Minimising Neg log like")
 parabolicMinimiser(mixAng,likelihoodVals)
 x=parabolicMinimiser(mixAng,likelihoodVals)[0]
 y=parabolicMinimiser(mixAng,likelihoodVals)[1]
@@ -236,5 +246,91 @@ plt.ylabel("Negative Log Likelihood")
 plt.plot(mixAng,likelihoodVals)
 plt.plot(x,y,"x")
 
+plt.figure("test")
+parabolicMinimiser(E,noDecayProb)
+x1=parabolicMinimiser(E,noDecayProb)[0]
+y1=parabolicMinimiser(E,noDecayProb)[1]
+plt.plot(E,noDecayProb)
+plt.plot(x1,y1,"x")
+
+
 
 #%%
+#3.5 Find accuracy of fit result
+
+#By shifting the value of the NLL at the minimum by +0.5, the two corresponding
+#theta values's range give one standard deviation
+
+minX=parabolicMinimiser(mixAng,likelihoodVals)[0]
+minY=parabolicMinimiser(mixAng,likelihoodVals)[1]
+dictionary=parabolicMinimiser(mixAng,likelihoodVals)[2]
+xFinal=parabolicMinimiser(mixAng,likelihoodVals)[3]
+
+#def NLLshifterror(minY,xVals,yVals,dictionary):
+
+    #in order to find the values of x at y+-0.5, use the last lagrange polynomial used
+    #to find the minimum in the minimiser function
+    #newY=minY+0.5
+
+
+
+def NLLgaussianError(xVals,yVals,minX):
+    #by approximating the pdf as a gaussian, we can find the uncertainty of the measurement
+    #the best estimate of the min is found using our minimising function
+    N=len(xVals)
+    sigma=minX/np.sqrt(N)
+    print("The error of ", minX, "is +/- ", sigma)
+    return(sigma)
+
+NLLgaussianError(mixAng,likelihoodVals,minX)
+
+
+#%%
+#4.1 The Univariate Method
+
+from mpl_toolkits import mplot3d
+
+#first we need to code an oscillation probability function that takes into account
+#mix angle and square mass difference
+
+def NLL_varying(expdata,simdata,mixAng,diffSqrMass):
+        #create array to store negative Log Likelihood values for varying mix angles
+    likelihood=[]
+
+    #set the constants
+    L=295
+    E=np.arange(0.025,10,0.05)
+    for i in range(len(mixAng)):
+        #finding the array of probabilities for a specific mix ang (prob vals depend on E)
+        noDecayProb=noOscProb(E,mixAng[i],diffSqrMass[i],L)
+        NLLsum=0
+        OscillationEventRate=oscEventRate(noDecayProb,simdata)
+        for j in range(len(noDecayProb)):
+            m=expdata[j]
+            Lamda=OscillationEventRate[j]
+            if m==0:
+                NLLsum+=Lamda
+            else:
+                NLLsum+=(Lamda-m+(m*np.log(m/Lamda)))
+        likelihood.append(NLLsum)
+
+    return likelihood
+
+diffSqrMass=np.linspace(1e-3,2.4e-3,200)
+Likelihood_2d=NLL_varying(expData,simData,mixAng,diffSqrMass)
+
+plt.figure("NLL vs mix ang")
+plt.plot(diffSqrMass,Likelihood_2d)
+plt.xlabel("Diff sqr mass")
+plt.ylabel("NLL")
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(mixAng, diffSqrMass, Likelihood_2d)
+
+
+
+    
+
+#%%
+
